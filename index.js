d2354dcb042f08c91c6bf5bcd1598bcd7c0c85c3
keith-mikel <keith.mikel@e5sol.com>
2023-06-29 10:52:14 -0500

    sudo code

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 // TODO: Include packages needed for this application
+const fs = require('fs')
+const inquirer = require('inquirer')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+// project title
+
+// table of contents
+
+// description
+
+// install instructions
+
+// usage information
+
+// contribution guidelines
+
+// test instructions
+
+// list of license with badge
+
+// github link / username
+
+// email address
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(ReadMe.md, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
